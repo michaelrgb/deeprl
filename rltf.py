e458798d44c8f956ca65e1b492cfcfffb617e0ae
michaelrgb <michaelrgb@users.noreply.github.com>
2017-10-06 18:50:45 +0100

    Implement SARSA learning. Added tensorboard summaries.

--- rltf.py
+++ rltf.py
@@ -2,51 +2,93 @@ import tensorflow as tf, numpy as np
 from utils import *
 sess = tf.InteractiveSession()
 
-INPUT_DIM = [96, 96, 3]# gym
-
-def make_layer(x, conv_width, input_channels, output_channels):
-    W_conv = weight_variable([conv_width, conv_width, input_channels, output_channels])
-    b_conv = bias_variable([output_channels])
-    x = tf.nn.relu(conv2d(x, W_conv) + b_conv)
-    x = max_pool_2x2(x)
-    return x
-def make_net(x):
-    net = make_layer(x,   5, INPUT_DIM[-1], 32)
-    net = make_layer(net, 5, 32, 64)
-    net = make_layer(net, 5, 64, 128)
-    return net
-def make_layer_flat(x, input_size, input_channels, output_channels):
-    flat_size = input_size * input_size * input_channels
-    W_f = weight_variable([flat_size, output_channels])
-    b_f = bias_variable([output_channels])
-    x = tf.reshape(x, [-1, flat_size])
-    x = tf.nn.relu(tf.matmul(x, W_f) + b_f)
-    return x
-
 import gym
 env = gym.make('CarRacing-v0')
+INPUT_DIM = [96, 96, 3]
+def make_layer(x, conv_width, input_channels, output_channels):
+    with tf.name_scope('weights'):
+        W_conv = weight_variable([conv_width, conv_width, input_channels, output_channels])
+        variable_summaries(W_conv)
+    with tf.name_scope('bias'):
+        b_conv = bias_variable([output_channels])
+        variable_summaries(b_conv)
+    x = [lrelu(conv2d(i, W_conv) + b_conv) for i in x]
+    x = [max_pool(i, size=3, stride=2) for i in x]
+    return x
 
-x_image = tf.placeholder(DTYPE, shape=[None] + INPUT_DIM)
-y = tf.placeholder(DTYPE, shape=[None, 1])
-net = make_net(x_image)
-sess.run(tf.global_variables_initializer())
+def make_net(x):
+    with tf.name_scope('layer0'):
+        x = make_layer(x, 5, INPUT_DIM[-1], 32)
+    with tf.name_scope('layer1'):
+        x = make_layer(x, 5, 32, 64)
+    with tf.name_scope('layer2'):
+        x = make_layer(x, 5, 64, 128)
+    return x
+
+def make_layer_flat(x, input_size, input_channels, output_channels):
+    flat_size = input_size * input_size * input_channels
+    with tf.name_scope('layer_flat'):
+        with tf.name_scope('weights'):
+            W_f = weight_variable([flat_size, output_channels])
+            variable_summaries(W_f)
+        with tf.name_scope('bias'):
+            b_f = bias_variable([output_channels])
+            variable_summaries(b_f)
+        x = [tf.reshape(i, [-1, flat_size]) for i in x]
+        x = [lrelu(tf.matmul(i, W_f) + b_f) for i in x]
+    return x
+
+state_tf = tf.placeholder(DTYPE, shape=[None] + INPUT_DIM)
+state_next_tf = tf.placeholder(DTYPE, shape=[None] + INPUT_DIM)
+reward_tf = tf.placeholder(DTYPE, shape=[None, 1])
+net = make_net([state_tf, state_next_tf])
 
 # Calculate topmost convolution dimensionality
+sess.run(tf.global_variables_initializer())
 observation = env.reset()
-feed_dict = {x_image: [observation]}
-eval = net.eval(feed_dict)
-net = make_layer_flat(net, eval.shape[1], eval.shape[3], 1)
+feed_dict = {state_tf: [observation]}
+eval = net[0].eval(feed_dict)
+[q_tf, q_next_tf] = make_layer_flat(net, input_size=eval.shape[1], input_channels=eval.shape[3], output_channels=1)
 
-sess.run(tf.global_variables_initializer()) # Made more variables
-eval = net.eval(feed_dict)# test
+with tf.name_scope('loss'):
+    # SARSA
+    GAMMA = 0.9
+    q_next_tf = tf.stop_gradient(q_next_tf)
+    loss_tf = (reward_tf + GAMMA*q_next_tf - q_tf)
+    loss_tf = loss_tf * loss_tf
+    #loss_tf = tf.pow(loss_tf, 2)
+    loss_sum = tf.reduce_sum(loss_tf)
 
-loss = tf.reduce_sum((y - net)**2)
-train_step = tf.train.GradientDescentOptimizer(0.005).minimize(loss)
+    loss_argmin = tf.argmin(loss_tf, 0)
+
+    tf.summary.histogram('histogram', loss_tf)
+    tf.summary.scalar('sum', loss_sum)
+
+ER_SIZE = 50
+#er_state = tf.get_variable("er_state", [ER_SIZE] + INPUT_DIM) # FIXME: use variables later
+#er_reward = tf.get_variable("er_reward", [ER_SIZE])
+er_state = np.zeros([ER_SIZE] + INPUT_DIM)
+er_state_next = np.zeros([ER_SIZE] + INPUT_DIM)
+er_reward = np.zeros([ER_SIZE, 1])
+# Random reward values to check we are actually modifying weights
+#er_reward = np.random.standard_normal([ER_SIZE, 1])
+
+sess.run(tf.global_variables_initializer())
+feed_dict = {state_tf: er_state, state_next_tf: er_state_next, reward_tf: er_reward}
+eval = q_tf.eval(feed_dict)# TEST
+
+opt = tf.train.GradientDescentOptimizer(0.00000001)
+#grads = opt.compute_gradients(loss_sum)
+train_step = opt.minimize(loss_sum)
+
+merged = tf.summary.merge_all()
+train_writer = tf.summary.FileWriter('/tmp/train', sess.graph)
 
 def gym_loop():
     observation = env.reset()
     state = observation
 
+    frame = 0
     while 1:
         env.render('human')# current display
         #rgb_video = env.render('rgb_array')
@@ -54,11 +96,20 @@ def gym_loop():
         action = env.action_space.sample() # take a random action
         observation, reward, done, info = env.step(action)
 
-        feed_dict={x_image: [state], y: [[reward]]}
-        sess.run(train_step, feed_dict)
+        er_index = frame
+        if frame >= ER_SIZE:
+            summary, loss_sum_eval, loss_argmin_eval, _ = sess.run([merged, loss_sum, loss_argmin, train_step], feed_dict)
+            er_index = loss_argmin_eval[0]
+            print(loss_sum_eval, er_index)
+            #loss_eval = sess.run(loss_tf, feed_dict); print(loss_eval)
 
-        loss_eval = loss.eval(feed_dict)
-        print(reward, loss_eval)
+            train_writer.add_summary(summary, frame)
+
+        er_state[er_index] = state
+        er_state_next[er_index] = observation
+        er_reward[er_index] = reward
+
 
         state = observation
+        frame += 1
 gym_loop()
--- utils.py
+++ utils.py
@@ -1,13 +1,29 @@
 import tensorflow as tf, numpy as np
 DTYPE = tf.float32
 
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+def max_pool(x, size=2, stride=2):
+    return tf.nn.max_pool(x, ksize=[1, size, size, 1],
+                          strides=[1, stride, stride, 1], padding='SAME')
+def lrelu(x):
+    return tf.maximum(x, x*0.000001)
+
 def weight_variable(shape):
     return tf.Variable(initial_value=tf.truncated_normal(shape, stddev=0.1))
 def bias_variable(shape):
     return weight_variable(shape)
     return tf.Variable(initial_value=tf.constant(0.1, shape=shape))
+
