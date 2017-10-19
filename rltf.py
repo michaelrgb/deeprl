153b3d62f90a1db5869c68d70a9759f7eaf90fda
michaelrgb <michaelrgb@users.noreply.github.com>
2017-10-19 22:40:58 +0100

    TD with gradient correction (TDC), to stop Q-function weight divergence.

--- rltf.py
+++ rltf.py
@@ -1,134 +1,227 @@
+GAMMA = 0.9
+STATE_FRAMES = 4
+ER_SIZE = 100
+GRADIENT_CORRECTION = True
+STATE_FEATURE_SIZE = 1000
+LEARNING_RATE_LINEAR = 1e-6/STATE_FEATURE_SIZE
+LEARNING_RATE_CONV = 1e-10
+
+SAVE_SUMMARIES = False
+RENDER_ENV = False
+SHOW_OBS = False
+MAX_EPISODE_STATES = 50
+TEST_ENV = False
+if TEST_ENV:
+    ER_SIZE = 1
+    STATE_FRAMES = 1
+    MAX_EPISODE_STATES = 2
+
 import tensorflow as tf, numpy as np
 from utils import *
-sess = tf.InteractiveSession()
 
-MAX_EPISODE_TRANSITIONS = 50
-STATE_FRAMES = 4
-FRAME_DIM = [96, 96, 3]
-STATE_DIM = FRAME_DIM[:2] + [FRAME_DIM[-1]*STATE_FRAMES]
+if TEST_ENV:
+    FRAME_DIM = [1]
+    ACTION_DIM = 1
+    class TestEnv:
+        def reset(self):
+            self.state = [1]
+            return self.state
+        def render(self): pass
+        def step(self, action):
+            self.state = [2]# Only initial and second state
+            reward = 0.
+            done = False
+            return self.state, reward, done, {}
+    env = TestEnv()
+else:
+    FRAME_DIM = [96, 96, 3]
+    ACTION_DIM = 3
+    import gym
+    env = gym.make('CarRacing-v0')
 
-def make_layer(x, conv_width, input_channels, output_channels):
+STATE_DIM = FRAME_DIM[:]
+STATE_DIM[-1] *= STATE_FRAMES
+env.reset(); env.render()# Gym seems to need at least 1 reset&render before we get valid observation
+
+def layer_conv(x, conv_width, input_channels, output_channels):
     with tf.name_scope('weights'):
         W_conv = weight_variable([conv_width, conv_width, input_channels, output_channels])
         variable_summaries(W_conv)
     with tf.name_scope('bias'):
-        b_conv = bias_variable([output_channels])
+        b_conv = weight_variable([output_channels])
         variable_summaries(b_conv)
+    x = wrapList(x)
     x = [lrelu(conv2d(i, W_conv) + b_conv) for i in x]
     x = [max_pool(i, size=5, stride=2) for i in x]
-    return x
+
+    return x, [W_conv, b_conv]
+
+def layer_reshape_flat(x, conv_eval):
+    input_size = conv_eval.shape[1]
+    input_channels = conv_eval.shape[3]
+    flat_size = input_size * input_size * input_channels
+    x = wrapList(x)
+    x = [tf.reshape(i, [-1, flat_size]) for i in x]
+    return x, flat_size
 
 def make_net(x):
-    with tf.name_scope('layer0'):
-        x = make_layer(x, 5, STATE_DIM[-1], 32)
-    with tf.name_scope('layer1'):
-        x = make_layer(x, 5, 32, 64)
-    with tf.name_scope('layer2'):
-        x = make_layer(x, 5, 64, 128)
-    return x
+    if TEST_ENV:
+        # No convolution for test env
+        return x, []
 
-def make_layer_flat(x, input_size, input_channels, output_channels):
-    flat_size = input_size * input_size * input_channels
-    with tf.name_scope('layer_flat'):
-        with tf.name_scope('weights'):
-            W_f = weight_variable([flat_size, output_channels])
-            variable_summaries(W_f)
-        with tf.name_scope('bias'):
-            b_f = bias_variable([output_channels])
-            variable_summaries(b_f)
-        x = [tf.reshape(i, [-1, flat_size]) for i in x]
-        x = [lrelu(tf.matmul(i, W_f) + b_f) for i in x]
-    return x
+    with tf.name_scope('layer0'):
+        x, conv_weights = layer_conv(x, 5, STATE_DIM[-1], 32)
+    with tf.name_scope('layer1'):
+        x, w = layer_conv(x, 5, 32, 64)
+        conv_weights += w
+    with tf.name_scope('layer2'):
+        x, w = layer_conv(x, 5, 64, 128)
+        conv_weights += w
+    return x, conv_weights
+
+def layer_fully_connected(x, flat_size, outputs):
+    with tf.name_scope('weights'):
+        W_f = weight_variable([flat_size, outputs])
+        variable_summaries(W_f)
+    with tf.name_scope('bias'):
+        b_f = weight_variable([outputs])
+        variable_summaries(b_f)
+    x = wrapList(x)
+    x = [lrelu(tf.matmul(i, W_f) + b_f) for i in x]
+    return x, [W_f, b_f]
+
+def layer_linear_sum(x, flat_size, outputs, init_zeros):
+    with tf.name_scope('weights'):
+        W_f = weight_variable([flat_size, outputs], init_zeros)
+        variable_summaries(W_f)
+    x = wrapList(x)
+    x = [tf.matmul(i, W_f) for i in x]
+    return x, W_f
 
 tf.set_random_seed(0)
 
-ER_SIZE = 50
-state_tf = tf.placeholder(DTYPE, shape=[None] + STATE_DIM)
-er_state = tf.get_variable("er_state", [ER_SIZE] + STATE_DIM,           dtype=DTYPE, trainable=False)
-er_state_next = tf.get_variable("er_state_next", [ER_SIZE] + STATE_DIM, dtype=DTYPE, trainable=False)
-er_reward = tf.get_variable("er_reward", [ER_SIZE, 1],                  dtype=DTYPE, trainable=False)
+sess = tf.InteractiveSession()
+with tf.name_scope('experience_replay'):
+    er_state =      tf.Variable(tf.zeros([ER_SIZE] + STATE_DIM),    False, name='state')
+    er_next_state = tf.Variable(tf.zeros([ER_SIZE] + STATE_DIM),    False, name='next_state')
+    er_action =     tf.Variable(tf.zeros([ER_SIZE] + [ACTION_DIM]), False, name='action')
+    er_reward =     tf.Variable(tf.zeros([ER_SIZE, 1]),             False, name='reward')
 
-net = make_net([er_state, er_state_next])
+if TEST_ENV:
+    state_features = [er_state, er_next_state]
+    STATE_FEATURE_SIZE = STATE_DIM[-1]
+else:
+    conv_net, conv_weights = make_net([er_state, er_next_state])
+    # Calculate topmost convolution dimensionality to create fully-connected layer
+    sess.run(tf.global_variables_initializer())
+    conv_net, conv_flat_size = layer_reshape_flat(conv_net, conv_net[0].eval())
+    state_features, w = layer_fully_connected(conv_net, conv_flat_size, STATE_FEATURE_SIZE)
+    conv_weights += w
 
-# Calculate topmost convolution dimensionality
-sess.run(tf.global_variables_initializer())
-eval = net[0].eval(feed_dict={})
-[q_tf, q_next_tf] = make_layer_flat(net, input_size=eval.shape[1], input_channels=eval.shape[3], output_channels=1)
+with tf.name_scope('q_value'):
+    [q_tf, q_next_tf], q_weights = layer_linear_sum(state_features, STATE_FEATURE_SIZE, 1, False)
 
-with tf.name_scope('loss'):
+with tf.name_scope('td_error'):
     # SARSA
-    GAMMA = 0.9
-    q_next_tf = tf.stop_gradient(q_next_tf)
-    loss_tf = (er_reward + GAMMA*q_next_tf - q_tf)
-    loss_tf = loss_tf * loss_tf
-    #loss_tf = tf.pow(loss_tf, 2)
-    loss_sum = tf.reduce_sum(loss_tf)
+    td_error = er_reward + GAMMA*tf.stop_gradient(q_next_tf) - q_tf
 
-    loss_argmin = tf.argmin(loss_tf, 0)
+    td_error_sq = td_error**2
+    td_error_loss = td_error_sum = tf.reduce_sum(td_error_sq)
+    loss_argmin = tf.argmin(td_error_sq, 0)
+    tf.summary.scalar('loss', td_error_sum)
 
-    tf.summary.histogram('histogram', loss_tf)
-    tf.summary.scalar('sum', loss_sum)
+with tf.name_scope('td_approx'):
+    if GRADIENT_CORRECTION:
+        [td_approx], td_approx_weights = layer_linear_sum(state_features[0], STATE_FEATURE_SIZE, 1, True)
+        td_approx_sum = tf.reduce_sum((td_error - td_approx)**2)
+        tf.summary.scalar('loss', td_approx_sum)
+    else:
+        td_approx_sum = tf.constant(0.)
 
 sess.run(tf.global_variables_initializer())
-eval = q_tf.eval(feed_dict={})# TEST
 
-opt = tf.train.GradientDescentOptimizer(1e-8)
-#grads = opt.compute_gradients(loss_sum)
-train_step = opt.minimize(loss_sum)
+# Use auto-differentiation just for convolutional weights
+opt_conv = tf.train.GradientDescentOptimizer(LEARNING_RATE_CONV)
+opt_approx = tf.train.GradientDescentOptimizer(LEARNING_RATE_LINEAR)
+opt_td = tf.train.GradientDescentOptimizer(LEARNING_RATE_LINEAR/100)
+q_apply_ops = []
+if not TEST_ENV:
+    grads = opt_conv.compute_gradients(td_error_loss, var_list=conv_weights)
+    q_apply_ops += [opt_conv.apply_gradients(grads)]
 
-merged = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter('/tmp/train', sess.graph)
+def reshape_grad(x): return tf.reshape(-tf.reduce_sum(x, axis=0), [STATE_FEATURE_SIZE, 1])
+grad_q = td_error*state_features[0]
+approx_apply_op = []
+if GRADIENT_CORRECTION:
+    # TD error with gradient correction (TDC)
+    grad_q += -GAMMA*td_approx*state_features[1]
 
-def gym_loop():
-    import gym
-    env = gym.make('CarRacing-v0')
-    env.reset(); env.render()# Seem to need at least 1 reset&render before we get valid observation
+    # Learn to approximate the TD error
+    grad = (td_error - td_approx)*state_features[0]
+    approx_apply_op += [opt_approx.apply_gradients([(reshape_grad(grad), td_approx_weights)])]
+q_apply_ops += [opt_td.apply_gradients([(reshape_grad(grad_q), q_weights)])]
 
+if SAVE_SUMMARIES:
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('/tmp/train', sess.graph)
+
+def rl_loop():
     state = np.zeros([STATE_FRAMES] + FRAME_DIM, 'uint8')
-    prev_state = np.empty_like(state)
+    next_state = np.empty_like(state)
 
-    render_env = False
-    show_obs = False
     episode = 0
-    training_iter = 0
+    er_num_saves = 0
     while 1:
         # New episode
         observation = env.reset()
 
-        for trans in range(MAX_EPISODE_TRANSITIONS):
-            action = env.action_space.sample() # take a random action
-            action = [0, 1, 0]
+        for ep_state in range(MAX_EPISODE_STATES):
+            if TEST_ENV:
+                action = [0.]
+            else:
+                action = env.action_space.sample() # take a random action
+                action = [0, 1, 0]
 
-            reward_sum = 0.
+            next_reward = 0.
             for frame in range(STATE_FRAMES):
-                if show_obs:
-                    print('Frame', trans); imshow(observation)
-                if render_env:
+                if SHOW_OBS:
+                    print('Frame', ep_state, frame); imshow(observation)
+                if RENDER_ENV:
                     env.render()# Not needed for state_pixels
 
-                state[frame] = observation
+                next_state[frame] = observation
                 observation, reward, done, info = env.step(action)
-                reward_sum += reward
+                next_reward += reward
 
-            if trans > 0:
-                save_index = -1
-                if training_iter < ER_SIZE:
-                    save_index = training_iter
+            if ep_state > 0:
+                if er_num_saves < ER_SIZE:
+                    save_index = er_num_saves
                 else:
-                    summary, loss_sum_eval, loss_argmin_eval, _ = sess.run([merged, loss_sum, loss_argmin, train_step], feed_dict={})
-                    save_index = loss_argmin_eval[0]
-                    print(loss_sum_eval, save_index)
-                    #loss_eval = sess.run(loss_tf); print(loss_eval)
-                    train_writer.add_summary(summary, training_iter)
+                    ops = q_apply_ops + approx_apply_op
+                    if SAVE_SUMMARIES:
+                        ops += [merged]
+                    r = sess.run([loss_argmin, td_error_sum, td_approx_sum] + ops)
+                    save_index=r[0][0]
+                    save_index = er_num_saves % ER_SIZE
+                    r = dict(
+                        episode=episode,
+                        save_index=save_index,
+                        td_error_sum=r[1],
+                        td_approx_sum=r[2])
+                    print(r)
+                    #loss_eval = sess.run(td_error); print(loss_eval)
+                    if SAVE_SUMMARIES:
+                        summary = r[3]
+                        train_writer.add_summary(summary, er_num_saves)
 
-                if save_index != -1:# and training_iter < 100:
-                    sess.run([er_state[save_index].assign(prev_state.reshape(STATE_DIM)/255.),
-                             er_state_next[save_index].assign(state.reshape(STATE_DIM)/255.),
-                             er_reward[save_index].assign([reward_sum])])
+                sess.run([er_state[save_index].assign(state.reshape(STATE_DIM)/255.),
+                         er_next_state[save_index].assign(next_state.reshape(STATE_DIM)/255.),
+                         er_reward[save_index].assign([reward_sum])])
+                er_num_saves += 1
 
-            prev_state[:] = state
-            training_iter += 1
+            state, next_state = next_state, state
+            reward_sum = next_reward
 
         episode += 1
 
-gym_loop()
+rl_loop()
--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
 import tensorflow as tf, numpy as np
 DTYPE = tf.float32
 
+def wrapList(value):
+    return value if type(value) == list else [value]
+
 def variable_summaries(var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.scalar('max', tf.reduce_max(var))
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.histogram('histogram', var)
 
 def conv2d(x, W, padding='VALID'):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
@@ -21,11 +21,8 @@ def max_pool(x, size=4, stride=2, padding='VALID'):
 def lrelu(x):
     return tf.maximum(x, x*0.01)
 
-def weight_variable(shape):
-    return tf.Variable(initial_value=tf.truncated_normal(shape, stddev=0.1), dtype=DTYPE)
-def bias_variable(shape):
-    return weight_variable(shape)
-    return tf.Variable(initial_value=tf.constant(0.1, shape=shape), dtype=DTYPE)
+def weight_variable(shape, init_zeros=False):
+    return tf.Variable(initial_value=tf.zeros(shape) if init_zeros else tf.truncated_normal(shape, stddev=0.1))
 
 def imshow(nparray):
     import matplotlib.pyplot as plt
