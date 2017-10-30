62dd5157a629b85397c985f844fb3ea4c8fbf7b3
michaelrgb <michaelrgb@users.noreply.github.com>
2017-10-30 20:23:40 +0000

    Auto-diff for linear features of Q, which are multiplied by TD error.

--- rltf.py
+++ rltf.py
@@ -1,50 +1,41 @@
 # coding=utf8
 
-GAMMA = 0.9
+GAMMA = 0.99
 STATE_FRAMES = 4
 ER_SIZE = 1000
 GRADIENT_CORRECTION = True
 COPDAC = True
 STATE_FEATURES = 1000
-LEARNING_RATE_LINEAR = 1e-2
+FULLY_CONNECTED_LAYERS = 3
+LEARNING_RATE_LINEAR = 1e-5
 LEARNING_RATE_CONV = 1e-12
 MOMENTUM = 0.9
 
 SAVE_SUMMARIES = False
 SHOW_OBS = False
 MAX_EPISODE_STATES = 10000
-TEST_ENV = False
-if TEST_ENV:
-    ER_SIZE = 1
-    STATE_FRAMES = 1
-    MAX_EPISODE_STATES = 2
 
 import tensorflow as tf, numpy as np
 from utils import *
 
-if TEST_ENV:
-    FRAME_DIM = [1]
-    ACTION_DIM = 1
-    class TestEnv:
-        def reset(self):
-            self.state = np.array([1.])
-            return self.state
-        def render(self): pass
-        def step(self, action):
-            self.state = np.array([2.])# Only initial and second state
-            reward = 0.
-            done = False
-            return self.state, reward, done, {}
-    env = TestEnv()
-else:
-    FRAME_DIM = [96, 96, 3]
-    ACTION_DIM = 3
-    import gym
-    env = gym.make('CarRacing-v0')
+import gym
+ENV_NAME = '''
+CarRacing-v0
+MountainCar-v0
+'''.splitlines()[-1]
+env = gym.make(ENV_NAME)
+envu = env.unwrapped
+if ENV_NAME == 'CarRacing-v0':
     car_racing = gym.envs.box2d.car_racing
     car_racing.WINDOW_W = 800 # Default is huge
     car_racing.WINDOW_H = 600
+elif ENV_NAME == 'MountainCar-v0':
+    envu.max_speed *= 2 # Cant seem to get up mountain
+    STATE_FRAMES = 1
+FRAME_DIM = list(env.observation_space.shape)
+ACTION_DIM = (env.action_space.shape or [env.action_space.n])[0]
 
+CONV_NET = len(FRAME_DIM) == 3
 STATE_DIM = FRAME_DIM[:]
 STATE_DIM[-1] *= STATE_FRAMES
 
@@ -71,11 +62,7 @@ def layer_conv(x, conv_width, conv_stride, input_channels, output_channels):
     #x = [tf.nn.dropout(i, keep_prob=0.5) for i in x]
     return x, [W_conv, b_conv]
 
-def make_net(x):
-    if TEST_ENV:
-        # No convolution for test env
-        return x, []
-
+def make_conv(x):
     with tf.name_scope('layer0'):
         chan_out = 32
         x, w = layer_conv(x, 5, 2, STATE_DIM[-1], chan_out)
@@ -108,8 +95,7 @@ def layer_fully_connected(x, flat_size, outputs):
         variable_summaries(b_f)
     x = wrapList(x)
     x = [tf.matmul(i, W_f) + b_f for i in x]
-    # Reduce number of "active" features
-    x = [tf.nn.softmax(i) for i in x]
+    x = [lrelu(i) for i in x]
     return x, [W_f, b_f]
 
 def layer_linear_sum(x, inputs, outputs, init_zeros=False):
@@ -129,23 +115,28 @@ with tf.name_scope('experience_replay'):
 def init_vars(): sess.run(tf.global_variables_initializer())
 
 state_ph = tf.placeholder(tf.float32, [None] + STATE_DIM)
-with tf.name_scope('state_features'):
-    state_features = [er_state, er_next_state, state_ph]
-    if TEST_ENV:
-        STATE_FEATURES = STATE_DIM[-1]
-    else:
-        conv_net, conv_weights = make_net(state_features)
+with tf.name_scope('conv_net'):
+    states = [er_state, er_next_state, state_ph]
+    if CONV_NET:
+        conv_net, shared_weights = make_conv(states)
         # Calculate topmost convolution dimensionality to create fully-connected layer
         init_vars()
-        conv_net, conv_flat_size = layer_reshape_flat(conv_net, conv_net[0].eval())
-        state_features, w = layer_fully_connected(conv_net, conv_flat_size, STATE_FEATURES)
-        conv_weights += w
+        conv_net, num_flat_inputs = layer_reshape_flat(conv_net, conv_net[0].eval())
+    else:
+        shared_weights = []
+        conv_net, num_flat_inputs = states, STATE_DIM[0]
+with tf.name_scope('state_features'):
+    state_features = conv_net
+    for i in range(FULLY_CONNECTED_LAYERS):
+        state_features, w = layer_fully_connected(state_features, num_flat_inputs, STATE_FEATURES)
+        shared_weights += w
+        num_flat_inputs = STATE_FEATURES
 
 if COPDAC:
     with tf.name_scope('policy'):
         # a = μ_θ(s)
         [policy, policy_ph], policy_weights = layer_linear_sum([state_features[0], state_features[2]], STATE_FEATURES, ACTION_DIM)
-        [policy_delta], w_weights = layer_linear_sum(state_features[0], STATE_FEATURES, ACTION_DIM)
+        [policy_grad], w_weights = layer_linear_sum(state_features[0], STATE_FEATURES, ACTION_DIM)
 
 # Qw(s, a) = (a − μ_θ(s)) T ∇_θ[μ_θ(s)] T w + V_v(s)
 with tf.name_scope('state_value'):
@@ -165,7 +156,7 @@ with tf.name_scope('td_error'):
     td_error = er_reward + GAMMA*tf.stop_gradient(q_value[1]) - q_value[0]
 
     td_error_sq = td_error**2
-    td_error_loss = td_error_sum = tf.reduce_sum(td_error_sq)
+    td_error_sum = tf.reduce_sum(td_error_sq)
     loss_argmin = tf.argmin(td_error_sq, 0)
     tf.summary.scalar('loss', td_error_sum)
 
@@ -177,34 +168,50 @@ with tf.name_scope('td_approx'):
     else:
         td_approx_sum = tf.constant(0.)
 
-opt_conv = tf.train.RMSPropOptimizer(LEARNING_RATE_CONV, momentum=MOMENTUM)
 opt_approx = tf.train.RMSPropOptimizer(LEARNING_RATE_LINEAR, momentum=MOMENTUM)
 opt_td = tf.train.RMSPropOptimizer(LEARNING_RATE_LINEAR/4, momentum=MOMENTUM)
-opt_policy = tf.train.RMSPropOptimizer(LEARNING_RATE_LINEAR/8, momentum=MOMENTUM)
-apply_ops= []
-if not TEST_ENV:
-    # Use auto-differentiation just for convolutional weights
-    grad_conv = opt_conv.compute_gradients(td_error_loss, var_list=conv_weights)
-    apply_ops += [opt_conv.apply_gradients(grad_conv)]
+opt_policy = tf.train.RMSPropOptimizer(LEARNING_RATE_LINEAR/100, momentum=MOMENTUM)
+apply_ops = []
 
-def reshape_grad_s(x): return tf.reshape(-tf.reduce_sum(x, axis=0), [STATE_FEATURES, 1])
-def reshape_grad_sa(x): return tf.reshape(-tf.reduce_sum(x, axis=0), [STATE_FEATURES, ACTION_DIM])
+# compute_gradients() sums up gradients for all instances, whereas TDC requires a
+# multiple of features at s_t+1 to be subtracted from those at s_t.
+# Therefore use auto-diff to calculate linear "features" of the Q function,
+# and then multiply those features by the TD error etc using custom gradients.
+def replace_gradient(expr, custom_grad):
+    new_op_name = 'new_op_' + str(replace_gradient.counter)
+    replace_gradient.counter += 1
+    @tf.RegisterGradient(new_op_name)
+    def _grad_(op, grad):
+        return -custom_grad
+    g = tf.get_default_graph()
+    with g.gradient_override_map({"Identity": new_op_name}):
+        return tf.identity(expr)
+replace_gradient.counter = 0
 
-grad_v = td_error*state_features[0]
+repl = replace_gradient(q_value[0], td_error)
+td_weights = shared_weights + [baseline_weights]
+grad_s = opt_td.compute_gradients(repl, var_list=td_weights)
+if COPDAC:
+    grad_w = opt_td.compute_gradients(repl, var_list=shared_weights+[w_weights])
+    apply_ops += [opt_td.apply_gradients(grad_w)]
+
 if GRADIENT_CORRECTION:
     # TD error with gradient correction (TDC)
-    grad_v += -GAMMA*td_approx*state_features[1]
+    repl = replace_gradient(q_value[1], -GAMMA*td_approx)
+    grad_s2 = opt_td.compute_gradients(repl, var_list=td_weights)
+    for i in range(len(td_weights)):
+        grad_s[i] = (grad_s[i][0] + grad_s2[i][0], grad_s[i][1])
+apply_ops += [opt_td.apply_gradients(grad_s)]
 
-    # Learn to approximate the TD error
-    grad = (td_error - td_approx)*state_features[0]
-    apply_ops += [opt_approx.apply_gradients([(reshape_grad_s(grad), td_approx_weights)])]
-apply_ops += [opt_td.apply_gradients([(reshape_grad_s(grad_v), baseline_weights)])]
+# Approximate the TD error
+repl = replace_gradient(td_approx, td_error-td_approx)
+grad = opt_approx.compute_gradients(repl, var_list=shared_weights+[td_approx_weights])
+apply_ops += [opt_approx.apply_gradients(grad)]
+
 if COPDAC:
-    grad_w = tf.expand_dims(td_error, 2) * state_action_features
-    apply_ops += [opt_td.apply_gradients([(reshape_grad_sa(grad_w), w_weights)])]
-
-    grad_theta = tf.expand_dims(state_features[0], 2) * tf.expand_dims(policy_delta, 1)
-    apply_ops += [opt_policy.apply_gradients([(reshape_grad_sa(grad_theta), policy_weights)])]
+    repl = replace_gradient(policy, policy_grad)
+    grad = opt_policy.compute_gradients(repl, var_list=policy_weights)
+    apply_ops += [opt_policy.apply_gradients(grad)]
 
 init_vars()
 
@@ -213,20 +220,20 @@ if SAVE_SUMMARIES:
     apply_ops += [merged]
     train_writer = tf.summary.FileWriter('/tmp/train', sess.graph)
 
-policy_keyboard = True
+on_policy = False
 enable_training = True
 def setup_key_actions(a):
     from pyglet.window import key
     def key_press(k, mod):
-        global restart, policy_keyboard, enable_training
+        global restart, on_policy, enable_training
         if k==0xff0d: restart = True
         if k==key.LEFT:  a[0] = -1.0
         if k==key.RIGHT: a[0] = +1.0
         if k==key.UP:    a[1] = +1.0
         if k==key.DOWN:  a[2] = +0.8   # set 1.0 for wheels to block to zero rotation
-        if k==ord('k'):
-            policy_keyboard ^= 1
-            print('policy_keyboard:', policy_keyboard)
+        if k==ord('p'):
+            on_policy ^= 1
+            print('on_policy:', on_policy)
         if k==ord('t'):
             enable_training ^= 1
             print('enable_training:', enable_training)
@@ -235,43 +242,54 @@ def setup_key_actions(a):
         if k==key.RIGHT and a[0]==+1.0: a[0] = 0
         if k==key.UP:    a[1] = 0
         if k==key.DOWN:  a[2] = 0
-    window = env.unwrapped.viewer.window
+    window = envu.viewer.window
     window.on_key_press = key_press
     window.on_key_release = key_release
 
+ACTIONS_CSV = 'actions_' + ENV_NAME + '.csv'
+try: actions_csv = np.loadtxt(open(ACTIONS_CSV, 'rb'), delimiter=',', skiprows=0)
+except: actions_csv = []
+
 def rl_loop():
     env.reset(); env.render()# Gym seems to need at least 1 reset&render before we get valid observation
-    human_action = np.array([0.]*ACTION_DIM)
-    setup_key_actions(human_action)
+    keyboard_action = np.array([0.]*ACTION_DIM)
+    setup_key_actions(keyboard_action)
 
+    er_num_saves = 0
     state = np.zeros([STATE_FRAMES] + FRAME_DIM)
     next_state = np.empty_like(state)
     def trans_state(x):
-        dims = len(FRAME_DIM)
-        return x.transpose(range(1, dims) + [0, 3]).reshape(STATE_DIM)
+        if CONV_NET:
+            dims = len(FRAME_DIM)
+            x = x.transpose(range(1, dims) + [0, 3]) / 255.
+        return x.reshape(STATE_DIM)
 
-    def step_state(num_states):
-        if num_states in [0, MAX_EPISODE_STATES]:
+    def step_state(states_sofar):
+        if states_sofar in [0, MAX_EPISODE_STATES]:
             # New episode
             obs = env.reset()
-            num_states = 0
+            states_sofar = 0
 
-        next_reward = 0.
+        env_action = action if env.action_space.shape else np.argmax(action)
+        reward_sum = 0.
         for frame in range(STATE_FRAMES):
             if SHOW_OBS:
-                print('Frame', num_states, frame); imshow(obs)
+                print('Frame', states_sofar, frame); imshow(obs)
             env.render()
 
-            # Get obs from env.state instead of env.{reset,step}
-            obs = env.unwrapped.state / 255.
+            obs, reward, done, info = env.step(env_action)
             next_state[frame] = obs
             #imshow([obs, test_lcn(obs)])
-            obs, reward, done, info = env.step(action)
-            next_reward += reward
+            if ENV_NAME == 'MountainCar-v0':
+                done = False
+                # Mountain car env doesnt give any +reward
+                if obs[0] >= envu.goal_position:
+                    reward = 1000.
+                    done = True
+            reward_sum += reward
+        print('ER saves:', er_num_saves, 'Action taken:', action, 'Reward:', reward_sum)
+        return states_sofar + 1, reward_sum, done
 
-        return num_states + 1, next_reward
-
-    er_num_saves = 0
     training_its = 0
     def run_training_ops():
         r = sess.run([loss_argmin, td_error_sum, td_approx_sum] + apply_ops)
@@ -290,32 +308,56 @@ def rl_loop():
             train_writer.add_summary(summary, training_its)
         return save_index
 
-    num_states = 0
+    global on_policy
+    states_sofar = 0
     while 1:
-        action = human_action
-        if num_states > 0:
+        action = keyboard_action
+        if not env.action_space.shape:
+            # Convert a[0] values to one-hot vector
+            action = [1. if action[0]+1. == i else 0. for i in range(ACTION_DIM)]
+
+        if states_sofar > 0:
             # Calculate the policy actions:
             [policy_action] = sess.run(policy_ph, feed_dict={state_ph: [trans_state(state)]})
-            if not policy_keyboard:
+            if on_policy:
                 action = policy_action # On-policy
 
+        do_env_step = True
         do_training = enable_training and er_num_saves >= ER_SIZE
         if do_training:
             run_training_ops()
             training_its += 1
+            # The GL env slows down training
+            #if training_its % 10:
+            do_env_step = False
 
-            if not training_its % 5:
-                env.render()# Render needed for keyboard events
-        else:
-            num_states, next_reward = step_state(num_states)
-            if num_states >= 2:
+        if do_env_step:
+            if er_num_saves < len(actions_csv):
+                action = actions_csv[er_num_saves]
+
+            states_sofar, reward_sum, done = step_state(states_sofar)
+            if states_sofar >= 2:# and er_num_saves < ER_SIZE:
                 save_index = er_num_saves % ER_SIZE
+                if on_policy:
+                    # Keep all the demonstrated actions
+                    save_index = ER_SIZE/2 + er_num_saves % int(ER_SIZE/2)
                 sess.run([er_state[save_index].assign(trans_state(state)),
-                         er_next_state[save_index].assign(trans_state(next_state)),
-                         er_reward[save_index].assign([reward_sum])])
+                          er_next_state[save_index].assign(trans_state(next_state)),
+                          er_action[save_index].assign(action),
+                          er_reward[save_index].assign([reward_sum])])
                 #imshow([er_state[0][:,:,0:3].eval(), er_state[0][:,:,3:6].eval()])
                 er_num_saves += 1
 
+                if er_num_saves == ER_SIZE/2:
+                    np.savetxt(ACTIONS_CSV, er_action[0:er_num_saves].eval(), delimiter=',')
+                    # Switch to target policy
+                    on_policy = True
+
             state, next_state = next_state, state
-            reward_sum = next_reward
+            if done:
+                states_sofar = 0
+        else:
+            # Render needed for keyboard events
+            if not training_its % 5: env.render()
+
 rl_loop()
